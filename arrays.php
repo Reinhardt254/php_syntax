5c06129f16e90de1e98fc2be924c5fe39b56e401
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-05-24 16:26:27 +0300

    added arrays, objects and dataTypes

---
+++ arrays.php
@@ -0,0 +1,39 @@
+<?php
+// $cars = array("volvo", "BMW", "Toyota", "Isuzu");
+// $cars[3]="donkey";
+
+// echo $cars[3];
+
+// $colours = ["blue", "red", "green", "yellow", "orange"];
+
+// foreach( $colours as $col ){
+//    echo $col . "<br>";
+// }
+
+// function myFunction(){
+//    echo "Hello";
+// }
+
+// $myArr = array("Volvo", 15, ["apples", "bananas"], myFunction());
+
+// echo count($myArr);
+
+#=============Array Keys=================
+
+// $cars = [0 => "Volvo", 1 => "BMW", 2 => "Toyota"];
+
+// $myCar = [
+//    "brand" => "Ford",
+//    "model" => "Mustang",
+//    "year" => 1964
+// ];
+
+$cars = [];
+$cars[0] = "Volvo";
+$cars[1] = "BMW";
+$cars[2] = "Toyota";
+
+echo $cars[2];
+
+#php arrays.php
+ 
